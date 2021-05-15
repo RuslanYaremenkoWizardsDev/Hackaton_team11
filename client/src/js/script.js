4510860ffa88e054650844ad37e883ac4548af37
Slav <exemple@gmail.com>
2021-05-15 19:28:25 +0300

    Reistr

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -1,9 +1,11 @@
 import '../scss/style.scss';
 
 import hello from '../modules/hello';
+import auth from '../modules/req';
+import req from '../modules/req';
 
 
 window.addEventListener('DOMContentLoaded', () => {
     hello();
- 
-  })
+    req();
+  });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/'),
     },
     devServer: {
+        port: 4200,
         contentBase: path.resolve(__dirname, '/dist')
     },
     //watch: true,
