819ec0e288c81473ae43ffbda76fb62cdc532799
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-05-16 14:28:26 +0300

    clear all popup

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -1,6 +1,7 @@
 import '../scss/style.scss';
 import registr  from '../modules/req';
 import auth from '../modules/auth';
+import clearAll from '../modules/clearAll';
  
 document.addEventListener('DOMContentLoaded', ()=>{
 
@@ -8,6 +9,8 @@ if(window.location.pathname === "/index.html"){
     auth();
 }else if(window.location.pathname === "/regpage.html"){
     registr();
+}else if(window.location.pathname === "/admin.html"){
+    clearAll();
 }
 
 });
