121b87eae50e3ee6bb0340a4c9133ff39e39b733
jeid <jeid14@gmail.com>
2021-05-16 12:10:00 +0300

    add ecc

--- server/src/main/java/com/example/server/tournament/exception/controlleradvice/GameControllerAdvice.java
+++ server/src/main/java/com/example/server/tournament/exception/controlleradvice/GameControllerAdvice.java
@@ -6,6 +6,7 @@ import com.example.server.tournament.controller.RegisterUserController;
 import com.example.server.tournament.exception.*;
 import com.example.server.tournament.services.AddUserToTournament;
 import com.example.server.tournament.services.CreateGameService;
+import com.example.server.usercredentials.exception.InvalidFieldException;
 import com.example.server.usercredentials.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -64,6 +65,14 @@ public class GameControllerAdvice {
     }
     @ExceptionHandler(InvalidStatusTournamentException.class)
     public ResponseEntity<String> invalidStatusTournamentExistException(Exception e) {
+        log.info(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(e.getMessage());
+    }
+    @ExceptionHandler(InvalidFieldException.class)
+    public ResponseEntity<String> invalidFieldExistException(Exception e) {
         log.info(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
